b8d68e56c824104ef275ed30827617ed1fcdb14b
Ярослав Кисленков <Jarrpen@yandex.ru>
2023-03-30 02:17:17 +0300

    Sprint 3. Yatube

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -15,9 +15,9 @@ class PostAdmin(admin.ModelAdmin):
         'group',
     )
     list_editable = ('group',)
-    search_fields = ('text',) 
+    search_fields = ('text',)
     list_filter = ('pub_date',)
-    
+
     empty_value_display = '-пусто-'
 
 
